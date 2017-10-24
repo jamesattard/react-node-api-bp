5f42a13dff5e93d8943f987d18d8217f759f1cf6
James Attard <info@jamesattard.com>
2017-10-24 15:52:23 +0200

    setup react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -10,6 +11,10 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
