faa80c5e4999e5211834fbb68ac2b3deeb51a6d1
James Attard <info@jamesattard.com>
2017-10-26 08:41:23 +0200

    Added token check on application startup

--- client/src/index.js
+++ client/src/index.js
@@ -13,10 +13,18 @@ import Signup from './components/auth/signup';
 import Feature from './components/feature';
 import reducers from './reducers';
 
+import { AUTH_USER } from './actions/types'
+
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers)
+const token = localStorage.getItem('token')
+
+if (token) {
+  store.dispatch({type: AUTH_USER})
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router>
       <div>
         <Header />
